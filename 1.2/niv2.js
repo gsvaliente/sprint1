3433352824fd8706f272f4e161f04be4b0de3edd
Gabriel Valiente <gsvalient@gmail.com>
2022-09-14 11:20:00 +0200

    1.2 niv 2 completo

--- 1.2/niv2.js
+++ 1.2/niv2.js
@@ -1,3 +1,17 @@
-//EJERCICIO 2
+//EJERCICIO 1
 const func = atributo => ({ nombre: atributo });
 console.log(func('Gabriel'));
+
+//EJERCICIO 2
+class Persona {
+  constructor(nombre) {
+    this.nombre = nombre;
+  }
+
+  imprimirNombre() {
+    return this.nombre;
+  }
+}
+
+let persona = new Persona('Gabriel');
+console.log(persona.imprimirNombre());
