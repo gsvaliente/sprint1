fcc085e3b569eec01656ca50c8c814d09522f164
Gabriel Valiente <gsvalient@gmail.com>
2022-09-22 18:02:45 +0200

    1-3, niv 2-1 revisado

--- entrega1-3.js
+++ entrega1-3.js
@@ -4,12 +4,12 @@
 //create a function that returns a promise that calls the resolve or reject function
 const func = () => {
   return new Promise((resolve, reject) => {
-    let ran = Math.floor(Math.random() * 10);
+    let ranNum = Math.floor(Math.random() * 10);
     // console.log(ran);
-    if (ran <= 5) {
-      resolve(`Random number ${ran} is lower or equal to 5`);
+    if (ranNum <= 5) {
+      resolve(`Random number passed: ${ranNum} is lower or equal to 5`);
     } else {
-      reject(`Random number ${ran} is higher than 5`);
+      reject(`Random number failed: ${ranNum} is higher than 5`);
     }
   });
 };
@@ -22,67 +22,60 @@ func()
   });
 
 //EJERCICIO 2
-// const ejercicio2 = (algo, cb) => {
-//   cb(algo);
-// };
-
-// ejercicio2(5, (num) => {
-//   let doblar = num * 2;
-//   console.log(`${doblar} es la multiplicacion de ${num} por 2`);
-// });
+//crea una arrow function que reciba un parametro y una cb
+const arrow = (para, cb) => {};
 
 // //NIVEL 2
 // //EJERCICIO 1
-// let employees = [
-//   {
-//     id: 1,
-//     name: 'Linux Torvalds',
-//   },
-//   {
-//     id: 2,
-//     name: 'Bill Gates',
-//   },
-//   {
-//     id: 3,
-//     name: 'Jeff Bezos',
-//   },
-// ];
-// let salaries = [
-//   {
-//     id: 1,
-//     salary: 4000,
-//   },
-//   {
-//     id: 2,
-//     salary: 1000,
-//   },
-//   {
-//     id: 3,
-//     salary: 2000,
-//   },
-// ];
+let employees = [
+  {
+    id: 1,
+    name: 'Linux Torvalds',
+  },
+  {
+    id: 2,
+    name: 'Bill Gates',
+  },
+  {
+    id: 3,
+    name: 'Jeff Bezos',
+  },
+];
+let salaries = [
+  {
+    id: 1,
+    salary: 4000,
+  },
+  {
+    id: 2,
+    salary: 1000,
+  },
+  {
+    id: 3,
+    salary: 2000,
+  },
+];
 
-// const getEmployee = (id) => {
-//   return new Promise((resolve, reject) => {
-//     if (employees[0].id === id) {
-//       resolve(employees[0].name);
-//     } else if (employees[1].id === id) {
-//       resolve(employees[1].name);
-//     } else if (employees[2].id === id) {
-//       resolve(employees[2].name);
-//     } else {
-//       reject('User does not exist');
-//     }
-//   });
-// };
+const getEmployee = (id) => {
+  return new Promise((resolve, reject) => {
+    let i = 0;
+    while (i < employees.length) {
+      if (employees[i].id === id) {
+        resolve(employees[i++].name);
+      }
+      i++;
+    }
+    reject('User was not found');
+  });
+};
 
-// getEmployee(1)
-//   .then((name) => {
-//     console.log(`Employee's name: ${name}`);
-//   })
-//   .catch((name) => {
-//     console.log('Error: ' + name);
-//   });
+getEmployee(3)
+  .then((name) => {
+    console.log(`Employee's name: ${name}`);
+  })
+  .catch((name) => {
+    console.log('Error: ' + name);
+  });
 
 // //EJERCICIO 2
 // const getSalary = (obj) => {
