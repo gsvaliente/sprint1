4ecf071fe49685712ca3c6962706a994319f11ab
Gabriel Valiente <gsvalient@gmail.com>
2022-09-27 10:28:22 +0200

    nivel 1 1-5 completo

---
+++ entrega1-5.js
@@ -0,0 +1,31 @@
+'use strict';
+////NIVEL 1
+//EJERCICIO 1
+const fs = require('fs');
+const zlib = require('zlib');
+
+//WRITE IN A NEW FILE
+const write = (mensaje) => {
+  fs.writeFileSync('writeMe.txt', mensaje);
+};
+write('Hola que tal!');
+
+//EJERCICIO 2
+//READ THE FILE
+const print = (dir) => {
+  const read = fs.readFileSync(`${dir}`, 'utf-8');
+  console.log(read);
+};
+print('writeMe.txt');
+
+//EJERCICIO 3
+//COMPRESS FILE
+const compress = (file) => {
+  const gzip = zlib.createGzip();
+  const inp = fs.createReadStream(file);
+  const out = fs.createWriteStream(file + '.gz');
+  inp.pipe(gzip).pipe(out);
+};
+compress('writeMe.txt');
+
+////NIVEL 2
