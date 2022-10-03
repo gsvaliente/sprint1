999200199cfa29711d9c58f560f9275abd952612
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2022-10-03 10:00:51 +0200

    Delete test1-2.test.js

--- __tests__/test1-2.test.js
+++
@@ -1,8 +0,0 @@
-// Create an arrow function that receives a parameter and a callback function and pass the function a message or another (which will be printed to the console) depending on the parameter received.
-const func = require('../app/test1-2');
-
-describe('functionality of arrow function', () => {
-  it('tests to see if the function is in fact a function', () => {
-    expect(typeof func).toBe('function');
-  });
-});
